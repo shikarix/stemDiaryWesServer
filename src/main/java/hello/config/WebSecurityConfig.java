561e936b01023cc9004e492a3c09ab0e1e0b7682
Виталий <vadim.eremin.2005@mail.ru>
2019-10-26 16:20:08 +0600

    успешный вход через сторонний шаблон login-сайта

--- src/main/java/hello/config/WebSecurityConfig.java
+++ src/main/java/hello/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package hello.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    @Bean
     protected UserDetailsService userDetailsService(){
         UserDetails user = User.withDefaultPasswordEncoder().username("vadim.eremin.2005@mail.ru").password("123").roles("USER").build();
         return new InMemoryUserDetailsManager(user);
