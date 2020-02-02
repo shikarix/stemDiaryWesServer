f75b5bc1747e1e03aca158967c1209a5a1c09035
СТЕМ <vadim.eremin.2005@mail.ru>
2020-02-02 17:08:34 +0600

    доработка для отправления на сервер

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,20 @@
-#!/usr/bin
+#!/usr/bin/env bash
+
+mvn clean package
+
+echo 'Copy Files...'
+
+scp -i D:\testkey \
+    out/artifacts/StemDiary_jar/StemDiary.jar \
+    ubuntu@18.191.156.108:/home/ubuntu/stemDiaryWesServer
+
+echo 'Restart server'
+
+ssh -i D:\testkey ubuntu@18.191.156.108 << EOF
+
+pgrep java | xargs kill -9
+nohup java -jar StemDiary.jar > log.txt &
+
+EOF
+
+echo 'bye'
